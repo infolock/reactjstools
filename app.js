89a2af2749ba9141f4181c09b1e27fbea71bcc01
Jon Hibbard <jhibbard@gmail.com>
2016-01-03 13:08:30 -0500

    Minor code/whitespace cleanup..

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 "use strict";
 
+var _ = require( 'underscore' );
+var cookieParser = require( 'cookie-parser' );
+var compress = require( 'compression' );
+var express = require( 'express' );
 var http = require( 'http' );
+var methodOverride = require( 'method-override' );
+var morgan = require( 'morgan' );
 var path = require( 'path' );
 var url = require( 'url' );
-var express = require( 'express' );
-var compress = require( 'compression' );
-var methodOverride = require( 'method-override' );
-var cookieParser = require( 'cookie-parser' );
-var morgan = require('morgan');
+
 var app = module.exports = express();
-var _ = require('underscore');
 
 app.set( 'views', path.join( __dirname, 'views' ) );
 app.set( 'view engine', 'jade' );
@@ -22,7 +23,7 @@ app.use( function( req, res, next ) {
     res.header( 'Expires', '-1' );
     res.header( 'Pragma', 'no-cache' );
 
-	next();
+    next();
 });
 
 app.use( methodOverride() );
@@ -31,7 +32,7 @@ app.use( compress() );
 app.use( morgan( 'dev' ) );
 
 app.get( '/', function( req, res ) {
-	res.render( 'index', {} );
+    res.render( 'index', {} );
 });
 
 http.createServer( app ).listen( app.get( 'port' ) );
