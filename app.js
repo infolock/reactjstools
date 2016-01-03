f1761c2c442de9cd035eb083d8d412f6262791e3
Jon Hibbard <jhibbard@gmail.com>
2016-01-03 13:11:21 -0500

    Minor code/whitespace cleanup..

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var app = module.exports = express();
 app.set( 'views', path.join( __dirname, 'views' ) );
 app.set( 'view engine', 'jade' );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
-
 app.set( 'port', process.env.PORT || 3000 );
 
 app.use( function( req, res, next ) {
